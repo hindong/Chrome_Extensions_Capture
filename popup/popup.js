66b889020edd6500a9d530a70d3062a7370b0487
Milkis2022 <86216382+hindong@users.noreply.github.com>
2023-05-22 22:46:40 +0900

    popup 버튼 수정

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,9 @@
 
-document.getElementById('option').addEventListener('click',() =>{
+const capture_button = document.getElementById('capture');
+const video_button = document.getElementById('video');
+const option_button = document.getElementById('option');
+
+
+option_button.addEventListener('click',() =>{
     chrome.tabs.create({url: 'options/options.html'});
 });
