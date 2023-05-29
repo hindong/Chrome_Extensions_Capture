bcf9b597639a59ea132ba9284332313289b60df6
bu <86216382+hindong@users.noreply.github.com>
2023-05-30 02:32:03 +0900

    define fullscreencaputre

--- background.js
+++ background.js
@@ -1,1 +1,13 @@
 console.log('test background');
+
+
+
+  
+
+  
+  
+  
+  
+  
+  
+  
--- popup/popup.js
+++ popup/popup.js
@@ -11,5 +11,28 @@ option_button.addEventListener('click',() =>{
 
 
 capture_button.addEventListener('click', () => {
-
+    alert("test");
 });
+
+
+captureAll_button.addEventListener('click', () => {
+    chrome.tabs.captureVisibleTab(null, { format: "png" }, function(dataUrl) {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          return;
+        }
+        
+        // test
+        console.log("Captured image data URL:", dataUrl);
+        downloadImage(dataUrl, "captured_image.png");
+      });
+});
+
+
+function downloadImage(dataUrl, filename) {
+    const link = document.getElementById("downloadLink");
+    link.href = dataUrl;
+    link.download = filename;
+    link.click();
+}
+
